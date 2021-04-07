bfaeecb02462ec9f69ec0f86053abd05ece3af0c
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-04-07 14:29:55 +0500

    wip.

--- src/components/modal.svelte
+++ src/components/modal.svelte
@@ -23,9 +23,10 @@
   <div class="modal">
     <!-- TODO: Insert backdrop -->
     <div class="content text-blob">
-      <div class="modal__header">
+      <div class="modal-header">
         <button aria-label="close this popup" on:click={closeModal}>
           <svg
+            class="close"
             viewBox="0 0 14 14"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
